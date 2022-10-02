0c7c7f5f9beb1dd4595c1ca9e47f3158adfe1b60
harikishore1234 <93607885+harikishore1234@users.noreply.github.com>
2022-10-02 12:47:49 +0530

    Add files via upload

---
+++ exceptionalconcepts/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionalconcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter rhe first input");
+                int firstvalue=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the second input");
+                int secondvalue=Convert.ToInt32(Console.ReadLine());
+                int resultvalue = firstvalue / secondvalue;
+                Console.WriteLine("result:" + resultvalue);
+            }
+            catch(ArithmeticException y)
+            {
+                Console.WriteLine(y.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("free the resources");
+            }
+            Console.ReadLine();
+        }
+    }
+}
